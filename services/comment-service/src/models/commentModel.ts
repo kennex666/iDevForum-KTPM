3e9fb2e7676db6183bb355af5f740f345bda00c0
EchhOpp <ngthluan285@gmail.com>
2025-03-20 20:21:09 +0700

    add: model of comment service

---
+++ services/comment-service/src/models/commentModel.ts
@@ -0,0 +1,25 @@
+import { Schema, model, Document } from "mongoose";
+
+interface IComment extends Document {
+    commentId: string;
+    postId: string;
+    userId: string;
+    content: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const commentSchema = new Schema({
+    commentId: { type: String, required: true },
+    postId: { type: String, required: true },
+    userId: { type: String, required: true },
+    content: { type: String, required: true }
+},
+    { timestamps: true }
+);
+
+const CommentModel = model<IComment>("Comment", commentSchema);
+
+export default CommentModel;
+export { IComment };
+
