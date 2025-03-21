91c19b384d0fe0c9173169e8f5e2067b3b02d527
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-03-21 15:24:31 +0700

    add: server of post service

---
+++ services/post-service/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/database';
+import postRoute from './routes/postRoute';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// Add routes
+app.use('/api/post', postRoute);
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+	app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
