ac4c876748ed79a868e17afb078a6df0d323a787
phan dinh thai <phandinhthai012@gmail.com>
2025-03-21 15:34:24 +0700

    update: update model user

--- services/user-service/src/models/User.ts
+++ services/user-service/src/models/User.ts
@@ -1,9 +1,17 @@
 import { Schema, model, Document } from "mongoose";
 
 interface IUser extends Document {
-	username: string;
+	name: string,
+	role: number,
+	accountState: string,
+	coverPicture: string,
+	description: string,
+	username: string,
 	email: string;
+	title: string,
 	password: string;
+	profilePicture: string,
+	bio: string,
 	createdAt: Date;
 	updatedAt: Date;
 }
@@ -11,9 +19,25 @@ interface IUser extends Document {
 // Định nghĩa Schema
 const UserSchema = new Schema<IUser>(
 	{
-		username: { type: String, required: true, unique: true },
+		name: { type: String, required: true },
+		role: {
+			type: Number,
+			enum: [0, 1],
+			default: 0,
+		},
+		accountState: {
+			type: String,
+			enum: ["EXPIRED", "BANNED", "RESTRICTED", "ACTIVE", "WAIT_FOR_ACTIVATION"],
+			default: "WAIT_FOR_ACTIVATION",
+		},
+		coverPicture: { type: String },
+		description: { type: String },
+		username: { type: String, required: true, unique: false },
 		email: { type: String, required: true, unique: true },
+		title: { type: String },
 		password: { type: String, required: true },
+		profilePicture: { type: String },
+		bio: { type: String },
 	},
 	{
 		timestamps: true, // Tự động thêm createdAt & updatedAt
