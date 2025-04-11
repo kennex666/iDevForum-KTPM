52d713b0d1f620842a52bee7f605e4a6226ddc2b
Dương Thái Bảo <me@dtbao.io.vn>
2025-04-11 22:44:23 +0700

    update: res.status always 200

--- services/authenticate-service/src/routes/authRoutes.ts
+++ services/authenticate-service/src/routes/authRoutes.ts
@@ -10,11 +10,11 @@ router.post("/register", async (req: any, res: any) => {
 		const { name, email, password, repassword } = req.body;
 
 		if (!name || name.trim() === "")
-			return res.status(400).json({ errorCode: 502, errorMessage: "Name is required" });
+			return res.status(200).json({ errorCode: 502, errorMessage: "Name is required" });
 		if (!email || email.trim() === "")
-			return res.status(400).json({ errorCode: 503, errorMessage: "Email is required" });
+			return res.status(200).json({ errorCode: 503, errorMessage: "Email is required" });
 		if (!password || password.length < 6)
-			return res.status(400).json({ errorCode: 504, errorMessage: "Password must be at least 6 characters" });
+			return res.status(200).json({ errorCode: 504, errorMessage: "Password must be at least 6 characters" });
 		if (password !== repassword)
 			return res
 				.status(400)
@@ -32,11 +32,12 @@ router.post("/register", async (req: any, res: any) => {
 			accountState: "ACTIVE",
 		});
 		if (user.data)
-			res.status(201).json(user.data);
+			res.status(200).json(user.data);
 		else
-			res.status(400).json(user);
+			res.status(200).json(user);
 	} catch (error) {
-		res.status(500).json({ errorCode: 500, errorMessage: "Error creating user" });
+		console.log("Error creating user", error);
+		res.status(200).json({ errorCode: 500, errorMessage: "Error creating user" });
 	}
 });
 
@@ -44,13 +45,13 @@ router.get("/login", async (req: any, res: any) => {
 	try {
 		const user = await getUserByEmail(req.params.email);
 
-		if (!user) return res.status(404).json({ message: "User not found" });
+		if (!user) return res.status(200).json({ message: "User not found" });
 
 		if (user.password !== req.params.password) // Mockup
-			return res.status(401).json({ message: "Invalid credentials" });
+			return res.status(200).json({ message: "Invalid credentials" });
 		res.json(user);
 	} catch (error) {
-		res.status(500).json({ error: "Error fetching user" });
+		res.status(200).json({ error: "Error fetching user" });
 	}
 });
 
