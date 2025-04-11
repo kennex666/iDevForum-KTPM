7b414d0ea348a9c43b66b5f356e11eea61d12549
Dương Thái Bảo <me@dtbao.io.vn>
2025-04-11 21:25:01 +0700

    fix: createUser client method

--- services/authenticate-service/src/clients/user.client.ts
+++ services/authenticate-service/src/clients/user.client.ts
@@ -1,16 +1,25 @@
 // clients/post.client.ts
 import axios from "axios";
 
+// JWT Token embed in the request header
+
+
 const POST_SERVICE_URL =
-	process.env.POST_SERVICE_URL || "http://user-service:3006";
+	process.env.POST_SERVICE_URL || "http://api-gateway:3000";
 
 export const UserClient = {
 	createUser: async (data: any) => {
 		const res = await axios.post(`${POST_SERVICE_URL}/register`, data, {
 			headers: {
 				"Content-Type": "application/json",
+				"Authorization": `Bearer ${data.token}` // JWT token
 			},
 		});
 		return res.data;
 	},
+
+	getUser: async (id: string) => {
+		const res = await axios.get(`${POST_SERVICE_URL}/user/${id}`);
+		return res.data;
+	}
 };
