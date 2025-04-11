13455a01ea8c51ee664b4164f6c901d696e2d44b
EchhOpp <ngthluan285@gmail.com>
2025-04-11 15:07:23 +0700

    add: document for service file

--- services/file-service/src/services/fileService.ts
+++ services/file-service/src/services/fileService.ts
@@ -8,8 +8,6 @@ import { Request } from "express";
  */
 interface IFileService {
     uploadImage(data: any): Promise<IFile>;
-    uploadPdf(data: any): Promise<IFile>;
-    uploadVideo(data: any): Promise<IFile>;
 }
 
 /**
@@ -49,56 +47,6 @@ class FileService implements IFileService {
             return await file.save();
         } catch (error) {
             this.handleError(error, 'Không thể tải lên ảnh');
-            throw error;
-        }
-    }
-
-    /**
-     * Upload PDF file
-     */
-    async uploadPdf(data: any): Promise<IFile> {
-        try {
-            // Upload file to storage (S3)
-            const fileUrl = await uploadFile(data.file);
-
-            // Create file record with data from Multer and request
-            const file = new fileModel({
-                userId: data.userId || 'anonymous',
-                fileName: data.fileName || data.file.originalname,
-                fileNameOriginal: data.fileNameOriginal || data.file.originalname,
-                fileType: data.fileType || data.file.mimetype,
-                fileSize: data.fileSize || data.file.size,
-                fileUrl: fileUrl
-            });
-            
-            return await file.save();
-        } catch (error) {
-            this.handleError(error, 'Không thể tải lên PDF');
-            throw error;
-        }
-    }   
-
-    /**
-     * Upload video file
-     */
-    async uploadVideo(data: any): Promise<IFile> {
-        try {
-            // Upload file to storage (S3)
-            const fileUrl = await uploadFile(data.file);
-
-            // Create file record with data from Multer and request
-            const file = new fileModel({
-                userId: data.userId || 'anonymous',
-                fileName: data.fileName || data.file.originalname,
-                fileNameOriginal: data.fileNameOriginal || data.file.originalname,
-                fileType: data.fileType || data.file.mimetype,
-                fileSize: data.fileSize || data.file.size,
-                fileUrl: fileUrl
-            });
-            
-            return await file.save();
-        } catch (error) {
-            this.handleError(error, 'Không thể tải lên video');
             throw error;
         }
     }
