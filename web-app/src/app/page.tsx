3b5d762eb0209c72850b435bda37a26215b93636
EchhOpp <ngthluan285@gmail.com>
2025-04-21 23:20:26 +0700

    update: page home ( redirect to login - icon FaSignOutAlt on NavBar

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-	return (
-		<div className="flex flex-row min-h-screen">
-			<p>Whattt</p>
-		</div>
-	);
+    // Chuyển hướng đến /login
+    redirect("/login");
 }
