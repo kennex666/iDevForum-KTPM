7a6a8c30139975525ca460589aee34360ff4b5d2
EchhOpp <ngthluan285@gmail.com>
2025-04-25 11:11:40 +0700

    Update(api-gateway): add middlewares - add: authenticate, conditionAuthen for type method - update: conditionalAuthenticate in server.ts

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import { authenticate } from "./middlewares/authenticate";
+import conditionalAuthenticate from "./middlewares/conditionalAuthenticate";
 
 const app = express();
 const PORT = 3000;
@@ -28,6 +30,7 @@ app.use(
 );
 app.use(
 	"/api/comment",
+	conditionalAuthenticate(["POST", "PUT", "DELETE"]),
 	createProxyMiddleware({
 		target: "http://comment-service:3001",
 		changeOrigin: true,
